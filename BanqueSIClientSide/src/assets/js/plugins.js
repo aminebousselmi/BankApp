e3d9d3f63f19da89b43bdf039011cd888e6b2850
aminebousselmi <amine1231996@outlook.com>
2018-03-27 14:58:25 +0200

    Add files via upload

--- BanqueSIClientSide/src/assets/js/plugins.js
+++ BanqueSIClientSide/src/assets/js/plugins.js
@@ -256,81 +256,12 @@ $(function() {
                     $(this).addClass("steps_"+$(this).children("li").length);
                 });//end
                 
-                // This par of code used for example
-                if($("#wizard-validation").length > 0){
-                    
-                    var validator = $("#wizard-validation").validate({
-                            rules: {
-                                login: {
-                                    required: true,
-                                    minlength: 2,
-                                    maxlength: 8
-                                },
-                                password: {
-                                    required: true,
-                                    minlength: 5,
-                                    maxlength: 10
-                                },
-                                repassword: {
-                                    required: true,
-                                    minlength: 5,
-                                    maxlength: 10,
-                                    equalTo: "#password"
-                                },
-                                email: {
-                                    required: true,
-                                    email: true
-                                },
-                                name: {
-                                    required: true,
-                                    maxlength: 10
-                                },
-                                adress: {
-                                    required: true
-                                }
-                            }
-                        });
-                        
-                }// End of example
-                
+               
                 $(".wizard").smartWizard({                        
-                    // This part of code can be removed FROM
-                    onLeaveStep: function(obj){
-                        var wizard = obj.parents(".wizard");
-
-                        if(wizard.hasClass("wizard-validation")){
-                            
-                            var valid = true;
-                            
-                            $('input,textarea',$(obj.attr("href"))).each(function(i,v){
-                                valid = validator.element(v) && valid;
-                            });
-                                                        
-                            if(!valid){
-                                wizard.find(".stepContainer").removeAttr("style");
-                                validator.focusInvalid();                                
-                                return false;
-                            }         
-                            
-                        }    
-                        
-                        return true;
-                    },// <-- TO
+                
                     
                     //This is important part of wizard init
-                    onShowStep: function(obj){                        
-                        var wizard = obj.parents(".wizard");
-
-                        if(wizard.hasClass("show-submit")){
-                        
-                            var step_num = obj.attr('rel');
-                            var step_max = obj.parents(".anchor").find("li").length;
-
-                            if(step_num == step_max){                             
-                                obj.parents(".wizard").find(".actionBar .btn-primary").css("display","block");
-                            }                         
-                        }
-                        return true;                         
+                    onShowStep: function(obj){                      
                     }//End
                 });
             }            
